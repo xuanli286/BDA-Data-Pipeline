45600481feee42f0db53ece459c739359ca324d5
Xuanli <xuanli743@gmail.com>
2024-10-31 02:33:48 +0800

    add athena to terraform & link to Tableau

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # BDA-Data-Pipeline
+
+## Terraform
+1. cd to terraform
+2. Run the following commands in Terminal to set up the AWS services. <br>
+`terraform init` <br>
+`terraform apply` <br>
+3. Run the below command in Terminal to destroy the AWS services. <br>
+`terraform destroy`
+
+## Tableau
+1. Download Athena driver to connect to Tableau <br>
+https://help.tableau.com/current/pro/desktop/en-us/examples_amazonathena.htm
+2. Launch Tableau, select Amazon Athena under Connection and key in the following. <br>
+<b>Access Key ID:</b> `AWS_ACCESS_KEY_ID` (refer to the .env file under the backend folder)
+<b>Secret Access Key:</b> `AWS_SECRET_ACCESS_KEY`
--- terraform/module.tf
+++ terraform/module.tf
@@ -1,29 +1,161 @@
+# Athena Workgroup
+resource "aws_athena_workgroup" "athena_workgroup" {
+  name = "JsonQuery"
 
-# start s3
-# resource "aws_s3_bucket" "is459-project-data" {
-#   bucket = "is459-project-data"
-# }
+  configuration {
+    enforce_workgroup_configuration    = true
+    publish_cloudwatch_metrics_enabled = true
 
-# resource "aws_s3_bucket" "is459-project-output-data" {
-#   bucket = "is459-project-output-data"
-# }
-
-resource "aws_s3_bucket" "testing-athena" {
-  bucket = "testing-athena-lalala"
+    result_configuration {
+      output_location = "s3://is459-project-athena-output/"
+    }
+  }
 }
 
-
-# resource "aws_iam_policy_attachment" "glue_s3_access" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-#   roles      = [aws_iam_role.example.name]
-# }
-# end s3
-
-
-# start athena 
-resource "aws_athena_database" "example" {
-  name   = "athena_database"
-  bucket = aws_s3_bucket.testing-athena.bucket
-  comment = "our athene database"
+# Athena Database
+resource "aws_athena_database" "athena_db" {
+  name   = "s3jsondb"
+  bucket = "is459-project-athena-output"
 }
-# end athena
+
+# Structured Table in Athena Database for Kaggle dataset
+resource "aws_glue_catalog_table" "athena_table_kaggle" {
+  database_name = "s3jsondb"
+  name          = "kaggle"
+
+  table_type = "EXTERNAL_TABLE"
+
+  storage_descriptor {
+    columns {
+      name = "ActualElapsedTime"
+      type = "double"
+    }
+    columns {
+      name = "AirTime"
+      type = "double"
+    }
+    columns {
+      name = "ArrDelay"
+      type = "double"
+    }
+    columns {
+      name = "ArrTime"
+      type = "double"
+    }
+    columns {
+      name = "CRSArrTime"
+      type = "bigint"
+    }
+    columns {
+      name = "CRSDepTime"
+      type = "bigint"
+    }
+    columns {
+      name = "CRSElapsedTime"
+      type = "double"
+    }
+    columns {
+      name = "CancellationCode"
+      type = "string"
+    }
+    columns {
+      name = "Cancelled"
+      type = "bigint"
+    }
+    columns {
+      name = "CarrierDelay"
+      type = "double"
+    }
+    columns {
+      name = "DayOfWeek"
+      type = "bigint"
+    }
+    columns {
+      name = "DayofMonth"
+      type = "bigint"
+    }
+    columns {
+      name = "DepDelay"
+      type = "double"
+    }
+    columns {
+      name = "DepTime"
+      type = "double"
+    }
+    columns {
+      name = "Dest"
+      type = "string"
+    }
+    columns {
+      name = "Distance"
+      type = "double"
+    }
+    columns {
+      name = "Diverted"
+      type = "bigint"
+    }
+    columns {
+      name = "FlightNum"
+      type = "bigint"
+    }
+    columns {
+      name = "LateAircraftDelay"
+      type = "double"
+    }
+    columns {
+      name = "Month"
+      type = "bigint"
+    }
+    columns {
+      name = "NASDelay"
+      type = "double"
+    }
+    columns {
+      name = "Origin"
+      type = "string"
+    }
+    columns {
+      name = "SecurityDelay"
+      type = "double"
+    }
+    columns {
+      name = "TailNum"
+      type = "string"
+    }
+    columns {
+      name = "TaxiIn"
+      type = "double"
+    }
+    columns {
+      name = "TaxiOut"
+      type = "double"
+    }
+    columns {
+      name = "UniqueCarrier"
+      type = "string"
+    }
+    columns {
+      name = "WeatherDelay"
+      type = "double"
+    }
+    columns {
+      name = "Year"
+      type = "bigint"
+    }
+
+    location = "s3://is459-project-output-data/kaggle/"
+
+    input_format  = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat"
+
+    ser_de_info {
+      serialization_library = "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
+    }
+  }
+
+  parameters = {
+    "classification" = "parquet"
+  }
+
+  depends_on = [aws_athena_database.athena_db]
+}
