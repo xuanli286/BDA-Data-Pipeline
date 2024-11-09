d6a592c7037136c4f93f45cb8be95d8d91e1d1d7
Xuanli <xuanli743@gmail.com>
2024-11-09 20:22:30 +0800

    add weather data into athena table

--- terraform/module.tf
+++ terraform/module.tf
@@ -328,3 +328,181 @@ resource "aws_glue_catalog_table" "athena_table_carriers" {
 
   depends_on = [aws_athena_database.athena_db]
 }
+
+# Structured Table in Athena Database for Weather dataset
+resource "aws_glue_catalog_table" "athena_table_weather" {
+  database_name = "s3jsondb"
+  name          = "weather"
+
+  table_type = "EXTERNAL_TABLE"
+
+  storage_descriptor {
+    columns {
+      name = "Date"
+      type = "date"
+    }
+    columns {
+      name = "Year"
+      type = "int"
+    }
+    columns {
+      name = "Month"
+      type = "int"
+    }
+    columns {
+      name = "DayofMonth"
+      type = "int"
+    }
+    columns {
+      name = "UniqueCarrier"
+      type = "string"
+    }
+    columns {
+      name = "FlightNum"
+      type = "int"
+    }
+    columns {
+      name = "Origin"
+      type = "string"
+    }
+    columns {
+      name = "Dest"
+      type = "string"
+    }
+    columns {
+      name = "CRSDepTime"
+      type = "int"
+    }
+    columns {
+      name = "DepTime"
+      type = "double"
+    }
+    columns {
+      name = "DepDelay"
+      type = "double"
+    }
+    columns {
+      name = "ArrTime"
+      type = "double"
+    }
+    columns {
+      name = "ArrDelay"
+      type = "double"
+    }
+    columns {
+      name = "Cancelled"
+      type = "double"
+    }
+    columns {
+      name = "Diverted"
+      type = "double"
+    }
+    columns {
+      name = "AirTime"
+      type = "double"
+    }
+    columns {
+      name = "Distance"
+      type = "double"
+    }
+    columns {
+      name = "CarrierDelay"
+      type = "double"
+    }
+    columns {
+      name = "WeatherDelay"
+      type = "double"
+    }
+    columns {
+      name = "NASDelay"
+      type = "double"
+    }
+    columns {
+      name = "LateAircraftDelay"
+      type = "double"
+    }
+    columns {
+      name = "CRSArrTime"
+      type = "bigint"
+    }
+    columns {
+      name = "ActualElapsedTime"
+      type = "double"
+    }
+    columns {
+      name = "CRSElapsedTime"
+      type = "double"
+    }
+    columns {
+      name = "TaxiIn"
+      type = "double"
+    }
+    columns {
+      name = "TaxiOut"
+      type = "double"
+    }
+    columns {
+      name = "CancellationCode"
+      type = "string"
+    }
+    columns {
+      name = "SecurityDelay"
+      type = "double"
+    }
+    columns {
+      name = "temperature_2m"
+      type = "double"
+    }
+    columns {
+      name = "wind_speed_10m"
+      type = "double"
+    }
+    columns {
+      name = "precipitation"
+      type = "double"
+    }
+    columns {
+      name = "rain"
+      type = "double"
+    }
+    columns {
+      name = "snowfall"
+      type = "double"
+    }
+    columns {
+      name = "longitude"
+      type = "double"
+    }
+    columns {
+      name = "latitude"
+      type = "double"
+    }
+    columns {
+      name = "CRSDepHour"
+      type = "int"
+    }
+    columns {
+      name = "wlatitude_deg"
+      type = "double"
+    }
+    columns {
+      name = "wlongitude_deg"
+      type = "double"
+    }
+
+    location = "s3://is459-project-output-data/weather/kaggle/airline_with_weather"
+
+    input_format  = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat"
+
+    ser_de_info {
+      serialization_library = "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
+    }
+  }
+
+  parameters = {
+    "classification" = "parquet"
+  }
+
+  depends_on = [aws_athena_database.athena_db]
+}
